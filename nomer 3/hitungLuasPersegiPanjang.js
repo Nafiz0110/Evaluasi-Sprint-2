e38456859ce2f7bf1d4a8c18153ebe03824b5fd4
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-31 09:42:57 +0700

    Add files via upload

---
+++ nomer 3/hitungLuasPersegiPanjang.js
@@ -0,0 +1,7 @@
+function hitungLuasPersegiPanjang(panjang,lebar){
+    luaspesegipanjang = panjang * lebar;
+    document.write('Nilai Luas Persegi Panjang : ' + luaspesegipanjang);
+}
+var panjang = prompt('Masukan Nilai Panjangnya');
+var lebar = prompt('Masukan Nilai Lebarnya');
+hitungLuasPersegiPanjang(panjang,lebar);
