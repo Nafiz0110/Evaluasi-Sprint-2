31f71239dd44ca1eed9d364511bc8d549571dbc5
@Nafizzz <87061342+Nafiz0110@users.noreply.github.com>
2021-07-31 10:59:34 +0700

    Add files via upload

---
+++ nomer 4/logic.js
@@ -0,0 +1,18 @@
+for (var y = 1; y < 10; y++){
+    for (var x = 1; x < 10; x++){
+        if(y == x){
+            document.write(y * 2 - 1)
+        }else if(x == 9 - (y - 1)){
+            document.write(x * 2 - 2)
+        }else if (x == 5){
+            document.write(y * 2 - 1)
+        }else if (y == 5){
+            document.write(x * 2 - 2)
+        }
+        else{
+            document.write(" _ ")
+        }
+    }
+    document.write("<br>")
+        
+}
